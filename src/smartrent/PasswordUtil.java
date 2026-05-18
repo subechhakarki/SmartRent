747e6e0795b1ef545a491bc53d277aa30d3e3747
Rasty <shahrasty99@gmail.com>
2026-05-18 15:00:00 +0545

    feat(auth): initialize password cryptography utility structure

--- src/smartrent/PasswordUtil.java
+++ src/smartrent/PasswordUtil.java
@@ -1,24 +1,2 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package smartrent;
-import org.mindrot.jbcrypt.BCrypt;
-
-/**
- *
- * @author Subechha Karki
- */
-
-
-public class PasswordUtil {
-    private static final int COST_FACTOR = 12;
-
-    public static String hashPassword(String plainPassword) {
-        return BCrypt.hashpw(plainPassword, BCrypt.gensalt(COST_FACTOR));
-    }
-
-    public static boolean verifyPassword(String plainPassword, String hashedPassword) {
-        return BCrypt.checkpw(plainPassword, hashedPassword);
-    }
-}
+public class PasswordUtil {}
