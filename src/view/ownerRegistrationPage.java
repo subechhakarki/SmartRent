b7a9f79c86d5f6e716254e25a7a74b947c500bd0
Bibek Bidari <bibekbidari00@gmail.com>
2026-05-19 11:00:00 +0545

    feat(owner-reg): implement UI fields and constructors for owner registration

--- src/view/ownerRegistrationPage.java
+++ src/view/ownerRegistrationPage.java
@@ -1,7 +1,14 @@
 package view;
 public class ownerRegistrationPage extends javax.swing.JFrame {
+    private javax.swing.JLabel Head;
+    private javax.swing.JLabel Email;
+    private javax.swing.JTextField jTextField1;
     public ownerRegistrationPage() {
         initComponents();
     }
-    private void initComponents() {}
+    private void initComponents() {
+        Head = new javax.swing.JLabel();
+        Email = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+    }
 }
