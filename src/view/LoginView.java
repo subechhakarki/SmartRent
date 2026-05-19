f4a4b99be78ad8ceecebb29ce268c121b9f48d8c
Prashna <prashnapoudel261@gmail.com>
2026-05-19 15:00:00 +0545

    feat(auth): design username and password input components

--- src/view/LoginView.java
+++ src/view/LoginView.java
@@ -1,7 +1,14 @@
 package view;
 public class LoginView extends javax.swing.JFrame {
+    private javax.swing.JLabel lblTitle;
+    private javax.swing.JTextField txtEmail;
+    private javax.swing.JPasswordField txtPassword;
     public LoginView() {
         initComponents();
     }
-    private void initComponents() {}
+    private void initComponents() {
+        lblTitle = new javax.swing.JLabel();
+        txtEmail = new javax.swing.JTextField();
+        txtPassword = new javax.swing.JPasswordField();
+    }
 }
