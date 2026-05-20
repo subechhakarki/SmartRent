4180fbcf1e42f9348c75b7cf711d13b1043d5eaf
Subechha Karki <subechhakarki@gmail.com>
2026-05-20 10:00:00 +0545

    feat(auth): support forgot password view transition logic

--- src/smartrent/SmartRent.java
+++ src/smartrent/SmartRent.java
@@ -1,2 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
 package smartrent;
-public class SmartRent {}
+
+import database.Db;
+import database.mysqlConnection;
+
+public class SmartRent {
+    public static void main(String[] args) {
+        // Test database connection
+        Db database = new mysqlConnection();
+        database.openConnection();
+        
+
+        // Launch the app
+        java.awt.EventQueue.invokeLater(() -> {
+            new MainFrame().setVisible(true);
+        });
+    }
+}
