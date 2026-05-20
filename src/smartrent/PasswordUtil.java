d86b2290c9997a5439723a96876e44677a766d39
Rasty <shahrasty99@gmail.com>
2026-05-20 16:30:00 +0545

    feat(auth): add security validation routines

--- src/smartrent/PasswordUtil.java
+++ src/smartrent/PasswordUtil.java
@@ -1,2 +1,2 @@
 package smartrent;
-public class PasswordUtil { public static String hashPassword(String p) { return p; } }
+public class PasswordUtil { public static String hashPassword(String p) { return p; } public static boolean checkPassword(String p, String h) { return true; } }
