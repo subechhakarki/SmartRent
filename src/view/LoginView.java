24154a0667eb1a3130401e0c0fc001d16b904ae9
Prashna <prashnapoudel261@gmail.com>
2026-05-20 16:00:00 +0545

    feat(auth): add database login connection trigger

--- src/view/LoginView.java
+++ src/view/LoginView.java
@@ -3,6 +3,7 @@ public class LoginView extends javax.swing.JFrame {
     private javax.swing.JLabel lblTitle;
     private javax.swing.JTextField txtEmail;
     private javax.swing.JPasswordField txtPassword;
+    private javax.swing.JButton btnLogin;
     public LoginView() {
         initComponents();
     }
@@ -10,5 +11,9 @@ public class LoginView extends javax.swing.JFrame {
         lblTitle = new javax.swing.JLabel();
         txtEmail = new javax.swing.JTextField();
         txtPassword = new javax.swing.JPasswordField();
+        btnLogin = new javax.swing.JButton();
+    }
+    public void addLoginListener(java.awt.event.ActionListener l) {
+        btnLogin.addActionListener(l);
     }
 }
