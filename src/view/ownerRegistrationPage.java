da2cafa4d8b86b29016376574f248b361e8aeaec
Bibek Bidari <bibekbidari00@gmail.com>
2026-05-20 12:00:00 +0545

    feat(owner-reg): implement input validation and database listeners

--- src/view/ownerRegistrationPage.java
+++ src/view/ownerRegistrationPage.java
@@ -3,6 +3,7 @@ public class ownerRegistrationPage extends javax.swing.JFrame {
     private javax.swing.JLabel Head;
     private javax.swing.JLabel Email;
     private javax.swing.JTextField jTextField1;
+    private javax.swing.JButton CreateOwnerAccount;
     public ownerRegistrationPage() {
         initComponents();
     }
@@ -10,5 +11,9 @@ public class ownerRegistrationPage extends javax.swing.JFrame {
         Head = new javax.swing.JLabel();
         Email = new javax.swing.JLabel();
         jTextField1 = new javax.swing.JTextField();
+        CreateOwnerAccount = new javax.swing.JButton();
+    }
+    public void addCreateAccountListener(java.awt.event.ActionListener l) {
+        CreateOwnerAccount.addActionListener(l);
     }
 }
