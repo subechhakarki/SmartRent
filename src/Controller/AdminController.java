dc0f72cb6912e978e1a40f039932d343ccb1f435
Shasank <mistik10000p@gmail.com>
2026-05-25 10:00:00 +0545

    feat(admin): initialize dashboard approval controller structure

--- src/Controller/AdminController.java
+++ src/Controller/AdminController.java
@@ -1,13 +1,2 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Controller;
-
-/**
- *
- * @author Subechha Karki
- */
-public class AdminController {
-    
-}
+public class AdminController {}
