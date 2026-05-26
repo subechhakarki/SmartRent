26fe849326ed0617c4b6c7c4dc3ccb5c7fc243dd
Rasty <shahrasty99@gmail.com>
2026-05-26 16:00:00 +0545

    feat(listing): add my properties table grid UI

--- src/view/MyPropertiesView.java
+++ src/view/MyPropertiesView.java
@@ -1,2 +1,2 @@
 package view;
-public class MyPropertiesView extends javax.swing.JFrame { public MyPropertiesView() { initComponents(); } private void initComponents() {} }
+public class MyPropertiesView extends javax.swing.JFrame { private javax.swing.JPanel pnlGrid; public MyPropertiesView() { initComponents(); } private void initComponents() { pnlGrid = new javax.swing.JPanel(); } }
