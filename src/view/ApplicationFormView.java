1ed3241511245910bf22be82e10749c73543abe5
Prashna <prashnapoudel261@gmail.com>
2026-05-26 15:00:00 +0545

    feat(apply): add application model and database DAO fields

--- src/view/ApplicationFormView.java
+++ src/view/ApplicationFormView.java
@@ -1,2 +1,2 @@
 package view;
-public class ApplicationFormView extends javax.swing.JFrame { public ApplicationFormView() { initComponents(); } private void initComponents() {} }
+public class ApplicationFormView extends javax.swing.JFrame { private javax.swing.JTextField txtDate; public ApplicationFormView() { initComponents(); } private void initComponents() { txtDate = new javax.swing.JTextField(); } }
