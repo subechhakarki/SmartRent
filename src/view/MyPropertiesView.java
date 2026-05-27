62c8271f354a953f618d704b87427343fccf2ce9
Rasty <shahrasty99@gmail.com>
2026-05-27 16:30:00 +0545

    feat(listing): add edit and delete button hooks

--- src/view/MyPropertiesView.java
+++ src/view/MyPropertiesView.java
@@ -1,2 +1,2 @@
 package view;
-public class MyPropertiesView extends javax.swing.JFrame { private javax.swing.JPanel pnlGrid; public MyPropertiesView() { initComponents(); } private void initComponents() { pnlGrid = new javax.swing.JPanel(); } }
+public class MyPropertiesView extends javax.swing.JFrame { private javax.swing.JPanel pnlGrid; private javax.swing.JButton btnEdit; public MyPropertiesView() { initComponents(); } private void initComponents() { pnlGrid = new javax.swing.JPanel(); btnEdit = new javax.swing.JButton(); } }
