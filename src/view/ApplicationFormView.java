c3de3cc2ccea0973f5f793125aee148b5c897570
Prashna <prashnapoudel261@gmail.com>
2026-05-27 16:00:00 +0545

    feat(apply): add application controller business logic

--- src/view/ApplicationFormView.java
+++ src/view/ApplicationFormView.java
@@ -1,2 +1,2 @@
 package view;
-public class ApplicationFormView extends javax.swing.JFrame { private javax.swing.JTextField txtDate; public ApplicationFormView() { initComponents(); } private void initComponents() { txtDate = new javax.swing.JTextField(); } }
+public class ApplicationFormView extends javax.swing.JFrame { private javax.swing.JTextField txtDate; private javax.swing.JButton btnSubmit; public ApplicationFormView() { initComponents(); } private void initComponents() { txtDate = new javax.swing.JTextField(); btnSubmit = new javax.swing.JButton(); } }
