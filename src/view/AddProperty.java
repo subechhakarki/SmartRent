83180ed037eff3b20edd75b15e0028074ad2016d
Bibek Bidari <bibekbidari00@gmail.com>
2026-05-27 13:00:00 +0545

    feat(listing): add property image uploading button actions

--- src/view/AddProperty.java
+++ src/view/AddProperty.java
@@ -1,2 +1,2 @@
 package view;
-public class AddProperty extends javax.swing.JFrame { private javax.swing.JTextField txtRent; public AddProperty() { initComponents(); } private void initComponents() { txtRent = new javax.swing.JTextField(); } }
+public class AddProperty extends javax.swing.JFrame { private javax.swing.JTextField txtRent; private javax.swing.JButton btnImage; public AddProperty() { initComponents(); } private void initComponents() { txtRent = new javax.swing.JTextField(); btnImage = new javax.swing.JButton(); } }
