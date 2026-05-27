b79256ace3de41880fa0b337d12b6555f0f27cad
Shasank <mistik10000p@gmail.com>
2026-05-27 17:00:00 +0545

    feat(admin): integrate user suspend action handlers

--- src/view/UserManagementView.java
+++ src/view/UserManagementView.java
@@ -1,2 +1,2 @@
 package view;
-public class UserManagementView extends javax.swing.JFrame { private javax.swing.JTable tblUsers; }
+public class UserManagementView extends javax.swing.JFrame { private javax.swing.JTable tblUsers; private javax.swing.JButton btnSuspend; }
