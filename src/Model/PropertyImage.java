0b9d58f993fb00cd83692b1b14a4ce0f987787fc
Bibek Bidari <bibekbidari00@gmail.com>
2026-06-01 11:00:00 +0545

    feat(listing): create PropertyImage model skeleton

--- src/Model/PropertyImage.java
+++ src/Model/PropertyImage.java
@@ -1,13 +1,2 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Model;
-
-/**
- *
- * @author Subechha Karki
- */
-public class PropertyImage {
-    
-}
+public class PropertyImage {}
