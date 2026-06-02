a775ae1066835fa5277cc6a53a3ea80cb14f757b
Rasty <shahrasty99@gmail.com>
2026-06-02 15:00:00 +0545

    feat(ratings): add rating comment submission controllers

--- src/view/PropertyRatingsView.java
+++ src/view/PropertyRatingsView.java
@@ -1,2 +1,2 @@
 package view;
-public class PropertyRatingsView extends javax.swing.JFrame { private javax.swing.JComboBox cmbScore; public PropertyRatingsView() { initComponents(); } private void initComponents() { cmbScore = new javax.swing.JComboBox(); } }
+public class PropertyRatingsView extends javax.swing.JFrame { private javax.swing.JComboBox cmbScore; private javax.swing.JButton btnSubmit; public PropertyRatingsView() { initComponents(); } private void initComponents() { cmbScore = new javax.swing.JComboBox(); btnSubmit = new javax.swing.JButton(); } }
