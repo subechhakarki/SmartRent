6979773dbb2152a2ef4cc0c4f67fc611648510aa
Shasank <mistik10000p@gmail.com>
2026-06-02 16:00:00 +0545

    feat(saved-prop): add saved property database mappings

--- src/view/SavedPropertiesView.java
+++ src/view/SavedPropertiesView.java
@@ -1,2 +1,2 @@
 package view;
-public class SavedPropertiesView extends javax.swing.JFrame { public SavedPropertiesView() { initComponents(); } private void initComponents() {} }
+public class SavedPropertiesView extends javax.swing.JFrame { private javax.swing.JPanel pnlGrid; public SavedPropertiesView() { initComponents(); } private void initComponents() { pnlGrid = new javax.swing.JPanel(); } }
