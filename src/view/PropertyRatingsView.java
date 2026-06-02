041e103af0cefde8d7c75b5ee36b5f4425851233
Rasty <shahrasty99@gmail.com>
2026-06-02 14:00:00 +0545

    feat(ratings): add rating scores model and database tables

--- src/view/PropertyRatingsView.java
+++ src/view/PropertyRatingsView.java
@@ -1,2 +1,2 @@
 package view;
-public class PropertyRatingsView extends javax.swing.JFrame { public PropertyRatingsView() { initComponents(); } private void initComponents() {} }
+public class PropertyRatingsView extends javax.swing.JFrame { private javax.swing.JComboBox cmbScore; public PropertyRatingsView() { initComponents(); } private void initComponents() { cmbScore = new javax.swing.JComboBox(); } }
