c05a72e9612ee0c79a6de4caae3c6987aa43145a
Prashna <prashnapoudel261@gmail.com>
2026-06-02 13:00:00 +0545

    feat(apply): add applications status table

--- src/view/MyApplicationView.java
+++ src/view/MyApplicationView.java
@@ -1,2 +1,2 @@
 package view;
-public class MyApplicationView extends javax.swing.JFrame { public MyApplicationView() { initComponents(); } private void initComponents() {} }
+public class MyApplicationView extends javax.swing.JFrame { private javax.swing.JTable tblApps; public MyApplicationView() { initComponents(); } private void initComponents() { tblApps = new javax.swing.JTable(); } }
