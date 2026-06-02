ea90de6d0897b27daa6fef9da80d65b1dfc1c6c3
Shasank <mistik10000p@gmail.com>
2026-06-02 15:00:00 +0545

    feat(saved-prop): implement list grid for bookmarked listings

--- src/view/SavedPropertiesView.java
+++ src/view/SavedPropertiesView.java
@@ -1,7 +1,12 @@
 package view;
 public class SavedPropertiesView extends javax.swing.JFrame {
+    private javax.swing.JLabel lblDashboardHeader;
+    private javax.swing.JPanel pnlGrid;
     public SavedPropertiesView() {
         initComponents();
     }
-    private void initComponents() {}
+    private void initComponents() {
+        lblDashboardHeader = new javax.swing.JLabel();
+        pnlGrid = new javax.swing.JPanel();
+    }
 }
