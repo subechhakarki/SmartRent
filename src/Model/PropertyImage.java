09b9232d593e024d54d9c00d6c60d4e9af447573
Bibek Bidari <bibekbidari00@gmail.com>
2026-06-02 14:00:00 +0545

    feat(listing): finalize property image management model mappings

--- src/Model/PropertyImage.java
+++ src/Model/PropertyImage.java
@@ -1,2 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package Model;
-public class PropertyImage { private int imageId; private boolean isPrimary; }
+
+/**
+ *
+ * @author Subechha Karki
+ */
+public class PropertyImage {
+    
+}
