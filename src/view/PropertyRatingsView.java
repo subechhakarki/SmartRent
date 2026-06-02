86a63779ccbf44ed96fd352525103c95b2d8ad90
Rasty <shahrasty99@gmail.com>
2026-06-02 11:00:00 +0545

    feat(ratings): add interactive star rating selectors

--- src/view/PropertyRatingsView.java
+++ src/view/PropertyRatingsView.java
@@ -1,7 +1,12 @@
 package view;
 public class PropertyRatingsView extends javax.swing.JFrame {
+    private javax.swing.JLabel lblTitle;
+    private javax.swing.JComboBox cmbScore;
     public PropertyRatingsView() {
         initComponents();
     }
-    private void initComponents() {}
+    private void initComponents() {
+        lblTitle = new javax.swing.JLabel();
+        cmbScore = new javax.swing.JComboBox();
+    }
 }
