9d39c3bd6b09dd96976ebabdedaea0db8cfb9231
Rasty <shahrasty99@gmail.com>
2026-06-03 12:00:00 +0545

    feat(ratings): connect rating submission with Database controller

--- src/view/PropertyRatingsView.java
+++ src/view/PropertyRatingsView.java
@@ -2,11 +2,13 @@ package view;
 public class PropertyRatingsView extends javax.swing.JFrame {
     private javax.swing.JLabel lblTitle;
     private javax.swing.JComboBox cmbScore;
+    private javax.swing.JButton btnSubmitReview;
     public PropertyRatingsView() {
         initComponents();
     }
     private void initComponents() {
         lblTitle = new javax.swing.JLabel();
         cmbScore = new javax.swing.JComboBox();
+        btnSubmitReview = new javax.swing.JButton();
     }
 }
