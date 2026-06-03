6afd2e81f0e639fb204ead5c440ad0891ef14716
Shasank <mistik10000p@gmail.com>
2026-06-03 16:00:00 +0545

    feat(saved-prop): link bookmark view with SavedPropertyDAO loading

--- src/view/SavedPropertiesView.java
+++ src/view/SavedPropertiesView.java
@@ -2,11 +2,13 @@ package view;
 public class SavedPropertiesView extends javax.swing.JFrame {
     private javax.swing.JLabel lblDashboardHeader;
     private javax.swing.JPanel pnlGrid;
+    private javax.swing.JButton btnNavSavedProperties;
     public SavedPropertiesView() {
         initComponents();
     }
     private void initComponents() {
         lblDashboardHeader = new javax.swing.JLabel();
         pnlGrid = new javax.swing.JPanel();
+        btnNavSavedProperties = new javax.swing.JButton();
     }
 }
