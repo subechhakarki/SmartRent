b7acbdad2166c9fb566a3ba00eac11fb1f80936a
Prashna <prashnapoudel261@gmail.com>
2026-06-03 14:00:00 +0545

    feat(apply): integrate withdraw button actions

--- src/view/MyApplicationView.java
+++ src/view/MyApplicationView.java
@@ -1,2 +1,2 @@
 package view;
-public class MyApplicationView extends javax.swing.JFrame { private javax.swing.JTable tblApps; public MyApplicationView() { initComponents(); } private void initComponents() { tblApps = new javax.swing.JTable(); } }
+public class MyApplicationView extends javax.swing.JFrame { private javax.swing.JTable tblApps; private javax.swing.JButton btnWithdraw; public MyApplicationView() { initComponents(); } private void initComponents() { tblApps = new javax.swing.JTable(); btnWithdraw = new javax.swing.JButton(); } }
