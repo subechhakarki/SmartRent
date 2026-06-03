0333a75b6f73c0031907637fcad8525ead2786ba
Shasank <mistik10000p@gmail.com>
2026-06-03 17:00:00 +0545

    feat(saved-prop): add bookmark toggle handlers

--- src/view/SavedPropertiesView.java
+++ src/view/SavedPropertiesView.java
@@ -1,2 +1,2 @@
 package view;
-public class SavedPropertiesView extends javax.swing.JFrame { private javax.swing.JPanel pnlGrid; public SavedPropertiesView() { initComponents(); } private void initComponents() { pnlGrid = new javax.swing.JPanel(); } }
+public class SavedPropertiesView extends javax.swing.JFrame { private javax.swing.JPanel pnlGrid; private javax.swing.JButton btnUnbookmark; public SavedPropertiesView() { initComponents(); } private void initComponents() { pnlGrid = new javax.swing.JPanel(); btnUnbookmark = new javax.swing.JButton(); } }
