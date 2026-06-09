c49afd9d031d59bc1e955868ff4d08478dc73a0f
Subechha Karki <subechhakarki@gmail.com>
2026-06-09 10:00:00 +0545

    feat(lease): connect lease form with pdf generation helpers

--- src/view/LeaseFormView.java
+++ src/view/LeaseFormView.java
@@ -1,2 +1,2 @@
 package view;
-public class LeaseFormView extends javax.swing.JPanel { private javax.swing.JTextField txtRent; public LeaseFormView() { initComponents(); } private void initComponents() { txtRent = new javax.swing.JTextField(); } }
+public class LeaseFormView extends javax.swing.JPanel { private javax.swing.JTextField txtRent; private javax.swing.JButton btnSave; public LeaseFormView() { initComponents(); } private void initComponents() { txtRent = new javax.swing.JTextField(); btnSave = new javax.swing.JButton(); } }
